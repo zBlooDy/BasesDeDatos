9f71b6af680605e0d6fb2a0412d73b2196e838b3
zBlooDy <tperezgiacchetta@frba.utn.edu.ar>
2025-11-02 11:06:51 -0300

    Puntos 27 y 28

---
+++ SQL/Punto_27.sql
@@ -0,0 +1,58 @@
+--------------
+-- PUNTO 27 --
+--------------
+
+/*
+Escriba una consulta sql que retorne una estadística basada en la facturacion por año y
+envase devolviendo las siguientes columnas:
+
+- Año
+- Codigo de envase
+- Detalle del envase
+- Cantidad de productos que tienen ese envase
+- Cantidad de productos facturados de ese envase
+- Producto mas vendido de ese envase
+- Monto total de venta de ese envase en ese año
+- Porcentaje de la venta de ese envase respecto al total vendido de ese año
+
+Los datos deberan ser ordenados por año y dentro del año por el envase con más
+facturación de mayor a menor
+*/
+
+
+SELECT year(f.fact_fecha),
+enva_codigo, 
+enva_detalle,
+(SELECT COUNT(*) FROM Producto WHERE prod_envase = enva_codigo) '# Productos envase',
+COUNT(distinct prod_codigo) '# Productos facturados',
+(SELECT TOP 1 prod_codigo 
+FROM Producto 
+JOIN Item_Factura ON item_producto = prod_codigo
+JOIN Factura ON item_tipo+item_sucursal+item_numero = fact_tipo+fact_sucursal+fact_numero
+WHERE prod_envase = enva_codigo AND year(fact_fecha) = year(f.fact_fecha)
+GROUP BY prod_codigo
+ORDER BY COUNT(prod_codigo) desc
+) 'Prod. mas vendido',
+SUM(item_cantidad * item_precio) '# Total facturado',
+SUM(item_cantidad * item_precio) * 100 / (SELECT SUM(fact_total) FROM Factura WHERE year(fact_fecha) = year(f.fact_fecha)) '% Del año'
+
+FROM Factura f
+JOIN Item_Factura ON item_tipo+item_sucursal+item_numero = f.fact_tipo+f.fact_sucursal+f.fact_numero
+JOIN Producto ON prod_codigo = item_producto
+JOIN Envases ON prod_envase = enva_codigo
+GROUP BY year(f.fact_fecha), enva_codigo, enva_detalle
+ORDER BY year(f.fact_fecha), SUM(item_cantidad * item_precio) desc
+
+
+
+
+
+/*
+WHERE prod_envase = (SELECT TOP 1 prod_envase
+                    FROM Producto 
+                    JOIN Item_Factura ON prod_codigo = item_producto
+                    JOIN Factura ON item_tipo+item_sucursal+item_numero = fact_tipo+fact_sucursal+fact_numero
+                    WHERE year(fact_fecha) = year(f.fact_fecha) 
+                    GROUP BY prod_envase
+                    ORDER BY SUM(item_cantidad * item_precio) desc)
+*/
---
+++ SQL/Punto_28.sql
@@ -0,0 +1,43 @@
+--------------
+-- PUNTO 28 --
+--------------
+
+/*
+Escriba una consulta sql que retorne una estadística por Año y Vendedor que retorne las
+siguientes columnas:
+
+- Año.
+- Codigo de Vendedor
+- Detalle del Vendedor
+- Cantidad de facturas que realizó en ese año
+- Cantidad de clientes a los cuales les vendió en ese año.
+- Cantidad de productos facturados con composición en ese año
+- Cantidad de productos facturados sin composicion en ese año.
+- Monto total vendido por ese vendedor en ese año
+
+Los datos deberan ser ordenados por año y dentro del año por el vendedor que haya
+vendido mas productos diferentes de mayor a menor.
+*/
+
+SELECT year(f.fact_fecha), 
+empl_codigo, 
+LTRIM(RTRIM(empl_nombre)) + ' ' + LTRIM(RTRIM(empl_apellido)) 'Empleado',
+COUNT(distinct fact_numero) '# Facturas',
+COUNT(distinct fact_cliente) '# Clientes',
+
+(SELECT COUNT(*) FROM Factura 
+JOIN Item_Factura ON item_tipo+item_sucursal+item_numero = fact_tipo+fact_sucursal+fact_numero
+JOIN Composicion ON item_producto = comp_producto
+WHERE year(f.fact_fecha) = year(fact_fecha) AND fact_vendedor = empl_codigo) '# Prod con comp.',
+                        
+(SELECT COUNT(*) FROM Factura 
+JOIN Item_Factura ON item_tipo+item_sucursal+item_numero = fact_tipo+fact_sucursal+fact_numero
+WHERE year(f.fact_fecha) = year(fact_fecha) AND fact_vendedor = empl_codigo AND item_producto NOT IN (SELECT comp_producto FROM Composicion)) '# Prod sin comp.',
+
+SUM(item_cantidad * item_precio) 'Monto total vendido'
+
+FROM Factura f
+JOIN Item_Factura ON item_tipo+item_sucursal+item_numero = f.fact_tipo+f.fact_sucursal+f.fact_numero
+JOIN Empleado ON f.fact_vendedor = empl_codigo                                                                                                                                                                                          
+GROUP BY year(f.fact_fecha), empl_codigo, empl_nombre, empl_apellido
+ORDER BY year(f.fact_fecha), COUNT(distinct item_producto) desc
