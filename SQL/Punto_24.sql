adcd151ce6f6f2ceb3306e819094932599bf9f9a
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-11-03 18:26:30 -0300

    Fix punto 24

--- SQL/Punto_24.sql
+++ SQL/Punto_24.sql
@@ -16,12 +16,11 @@ El resultado deberá ser ordenado por las unidades facturadas descendente
 SELECT prod_codigo, prod_detalle, SUM(item_cantidad)
 FROM Factura
 JOIN Item_Factura ON item_tipo+item_sucursal+item_numero = fact_tipo+fact_sucursal+fact_numero
-JOIN Composicion ON item_producto = comp_producto
-JOIN Producto ON comp_producto = prod_codigo
+JOIN Producto ON item_producto = prod_codigo
 WHERE fact_vendedor IN (SELECT TOP 2 empl_codigo
 						FROM Empleado
 						ORDER BY empl_comision desc)
+					AND prod_codigo IN (SELECT comp_producto FROM Composicion)
 GROUP BY prod_codigo, prod_detalle
 HAVING COUNT(fact_tipo+fact_sucursal+fact_numero) >= 5
 ORDER BY SUM(item_cantidad) DESC
-
